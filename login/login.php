694a9c41e9152240b5af4b83be10f9371c40f37e
viehp <nguyenhaohiep9x@gmail.com>
2024-04-01 16:34:26 +0700

    login update

--- login/login.php
+++ login/login.php
@@ -4,29 +4,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ASM4 - Login</title>
-  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="./css/login.css">
 </head>
 <body>
-  <header>
-    <h1>ASM4 - Login</h1>
-  </header>
-  <main>
-    <div class="container">
-      <h2>Login</h2>
-      <?php
-        // Show any error messages if login fails
-        if (isset($_GET['error'])) {
-          echo "<p class='error'>Invalid username or password.</p>";
-        }
-      ?>
-      <form action="login_process.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required>
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required>
-        <button type="submit">Login</button>
-      </form>
+ 
+  <div id="handmaid" class="book">
+    <div class="gloss"></div>
+    <img class="cover" src="https://raw.githubusercontent.com/robole/artifice/main/shiny-book-reveal/img/cover.png">
+    <div class="description">
+      <h1>The Handmaid's Tale</h1>
+   <div>
+   <?php
+          // Show any error messages if login fails
+          if (isset($_GET['error'])) {
+            echo "<p class='error'>Invalid username or password.</p>";
+          }
+        ?>
+        <form action="login_process.php" method="post">
+          <label for="username">Username:</label>
+          <input type="text" name="username" id="username" required>
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="password" required>
+          <button type="submit">Login</button>
+        </form>
+   </div>
+        <h1>The Handmaid's Tale</h1>
+  <div class="rating">
+    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+  </div>
     </div>
-  </main>
+   
+  </div>
 </body>
 </html>
